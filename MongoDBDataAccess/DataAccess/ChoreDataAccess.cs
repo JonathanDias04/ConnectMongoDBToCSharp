4338d5ff2d5cbaaa1f46417e9fde3a2d5df97dd4
Jonathan Dias <dev2@jaasolucoes.com.br>
2022-08-25 13:11:21 -0300

    Add project files.

---
+++ MongoDBDataAccess/DataAccess/ChoreDataAccess.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDataAccess.Models;
+using MongoDB.Driver;
+
+namespace MongoDBDataAccess.DataAccess
+{
+    public class ChoreDataAccess
+    {
+        private const string ConnectionString = "mongodb://127.0.0.1:27017";
+        private const string DatabaseName = "chore_db";
+        private const string ChoreCollection = "chore_chart";
+        private const string UserCollection = "users";
+        private const string CHoreHistoryCollection = "chore_history";
+        
+        private IMongoCollection<T> ConnectToMongo<T>(in string collection)
+        {
+            var client = new MongoClient(ConnectionString);
+            var db = client.GetDatabase(DatabaseName);
+            return db.GetCollection<T>(collection);
+        }
+
+        public async Task<List<UserModel>> GetAllUsers()
+        {
+            var usersCollection = ConnectToMongo<UserModel>(UserCollection);
+            var results = await usersCollection.FindAsync(_ => true);
+            return results.ToList();
+        }
+
+        public async Task<List<ChoreModel>> GetAllChores()
+        {
+            var choreCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+            var results = await choreCollection.FindAsync(_ => true);
+            return results.ToList();
+        }
+
+        public async Task<List<ChoreModel>> GetAllChoresForAUser(UserModel user)
+        {
+            var choreCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+            var results = await choreCollection.FindAsync(
+                    chore => chore.AssignedTo.Id == user.Id);
+            return results.ToList();
+        }
+
+        public Task CreateUser(UserModel user)
+        {
+            var usersCollection = ConnectToMongo<UserModel>(UserCollection);
+            return usersCollection.InsertOneAsync(user);
+        }
+
+        public Task CreateChore(ChoreModel chore)
+        {
+            var choreCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+            return choreCollection.InsertOneAsync(chore);
+        }
+
+        public Task UpdateChore(ChoreModel chore)
+        {
+            var choreCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+            var filter = Builders<ChoreModel>.Filter.Eq("Id", chore.Id);
+            return choreCollection.ReplaceOneAsync(filter, chore, new ReplaceOptions { IsUpsert = true });
+        }
+
+        public Task DeleteChore(ChoreModel chore)
+        {
+            var choreCollection = ConnectToMongo<ChoreModel>(ChoreCollection);
+            return choreCollection.DeleteOneAsync(c => c.Id == chore.Id);
+        }
+    }
+}
---
+++ teste/Program.cs
@@ -0,0 +1,37 @@
+﻿using MongoDataAccess.Models;
+using MongoDB.Driver;
+using MongoDBDataAccess.DataAccess;
+using MongoDBDemo;
+
+//string connectionString = "mongodb://127.0.0.1:27017";
+//string databaseName = "simple_db";
+//string collectionName = "people";
+
+//var client = new MongoClient(connectionString);
+//var db = client.GetDatabase(databaseName);
+//var collection = db.GetCollection<PersonModel>(collectionName);
+
+//var person = new PersonModel { FirstName = "Jonathan", LastName = "Dias" };
+
+//await collection.InsertOneAsync(person);
+
+//var results = await collection.FindAsync(_ => true);
+
+//foreach (var item in results.ToList())
+//{
+//    Console.WriteLine($"{item.Id}: {item.FirstName} {item.LastName}");
+//}
+
+ChoreDataAccess model = new ChoreDataAccess();
+
+await model.CreateUser(new UserModel() { FirstName = "Jonathan", LastName = "Gonçalves Dias" });
+var users = await model.GetAllUsers();
+
+var chore = new ChoreModel()
+{
+    AssignedTo = users.First(),
+    ChoreText = "Cortar a grama",
+    FrequencyInDays = 4
+};
+
+await model.CreateChore(chore);
